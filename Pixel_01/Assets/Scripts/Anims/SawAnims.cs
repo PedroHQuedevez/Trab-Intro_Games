34baf66ea2d4b90481b39c6d56b47b9ed59ccb90
pedro <pedro.quedevez@edu.ufes.br>
2024-09-30 17:22:00 -0300

    trab

---
+++ Pixel_01/Assets/Scripts/Anims/SawAnims.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class SawAnims : MonoBehaviour
+{
+    Animator animator;
+    float moveSpeed = 1f;
+    [SerializeField]
+    Transform[] pointsToMove;
+    [SerializeField]
+    int startingPoint;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+        transform.position = pointsToMove[startingPoint].transform.position;
+    }
+
+    public void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, pointsToMove[startingPoint].transform.position, moveSpeed * Time.deltaTime);
+        if (transform.position == pointsToMove[startingPoint].transform.position)
+        {
+            startingPoint += 1;
+        }
+
+        if (startingPoint == pointsToMove.Length)
+        {
+            startingPoint = 0;
+        }
+    }
+}
---
+++ Pixel_01/Assets/Scripts/Anims/SawDeath.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SawDeath : MonoBehaviour
+{
+    [SerializeField]
+    GameObject deathAnim;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Saw")
+        {
+            MortePorContato();
+        }
+    }
+
+    void MortePorContato()
+    {
+        Instantiate(deathAnim, transform.position, Quaternion.identity);
+        transform.parent.gameObject.SetActive(false);
+    }
+}
